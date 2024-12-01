037dff281b23c4126334dfd6c71dd2850b16baa8
bryan-barreto <bryan94barreto@gmail.com>
2024-12-01 01:09:10 -0500

    day 1 part 2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace AOC2024
     {
         static void Main(string[] args)
         {
-            Day1.part1();
-            
+            // Day1.part1();
+            Day1.part2();
         }
     }
 }
--- solutions/day1.cs
+++ solutions/day1.cs
@@ -10,8 +10,8 @@ namespace Solutions
             int total = 0;
             List<int> left = new List<int>();
             List<int> right = new List<int>();
-            // StreamReader sr = new StreamReader("examples/day1-1.txt");
-            StreamReader sr = new StreamReader("problems/day1-1.txt");
+            // StreamReader sr = new StreamReader("examples/day1.txt");
+            StreamReader sr = new StreamReader("problems/day1.txt");
             while ((line = sr.ReadLine()) != null){
                 string[] pair = line.Split("   ");
                 int value1 = 0;
@@ -27,6 +27,34 @@ namespace Solutions
             foreach (var valueSet in sortedList){
                 int hold = Math.Abs(valueSet.First - valueSet.Second);
                 total += hold;
+            }
+            Console.WriteLine(total);
+        }
+
+        public static void part2(){
+            string line;
+            int total = 0;
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            // StreamReader sr = new StreamReader("examples/day1.txt");
+            StreamReader sr = new StreamReader("problems/day1.txt");
+            while ((line = sr.ReadLine()) != null){
+                string[] pair = line.Split("   ");
+                int value1 = 0;
+                int value2 = 0;
+                int.TryParse(pair[0], out value1);
+                int.TryParse(pair[1], out value2);
+                left.Add(value1);
+                right.Add(value2);
+            }
+            foreach (int value in left){
+                int totalInRight = 0;
+                foreach (int otherValue in right){
+                    if (value==otherValue){
+                        totalInRight += 1;
+                    }
+                }
+                total += value * totalInRight;
             }
             Console.WriteLine(total);
         }
