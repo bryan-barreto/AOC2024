0b208b33f97f8eda89c42514a66f1ad4c3b4c65b
bryan-barreto <bryan94barreto@gmail.com>
2024-12-02 01:39:39 -0500

    day 2 part 2 having trouble

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace AOC2024
         {
             // Day1.part1();
             // Day1.part2();
-            Day2.part1();
-            // Day2.part2();
+            // Day2.part1();
+            Day2.part2();
         }
     }
 }
--- solutions/day2.cs
+++ solutions/day2.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Solutions
 {
     public static class Day2
@@ -22,13 +24,14 @@ namespace Solutions
                 string isIncreasing = "";
                 string[] levelString = report.Split(" ");
                 int[] levelInt = Array.ConvertAll(levelString, a => int.Parse(a));
-                for (int x = 0; x < levelInt.Count()-1; x++)
+                for (int x = 0; x < levelInt.Count() - 1; x++)
                 {
                     if (!isSafe)
                     {
                         continue;
                     }
-                    if (Math.Abs(levelInt[x]-levelInt[x+1])>3){
+                    if (Math.Abs(levelInt[x] - levelInt[x + 1]) > 3)
+                    {
                         isSafe = false;
                         continue;
                     }
@@ -38,7 +41,7 @@ namespace Solutions
                         {
                             isIncreasing = "increasing";
                         }
-                        else if(levelInt[x] > levelInt[x + 1])
+                        else if (levelInt[x] > levelInt[x + 1])
                         {
                             isIncreasing = "decreasing";
                         }
@@ -52,12 +55,91 @@ namespace Solutions
                     {
                         continue;
                     }
-                    else if(levelInt[x] > levelInt[x + 1] && isIncreasing == "decreasing")
+                    else if (levelInt[x] > levelInt[x + 1] && isIncreasing == "decreasing")
                     {
                         continue;
                     }
-                    else{
+                    else
+                    {
                         isSafe = false;
+                    }
+                }
+                if (isSafe)
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine(total);
+        }
+
+        public static void part2()
+        {
+            int total = 0;
+            string report;
+            // StreamReader sr = new StreamReader("examples/day2.txt");
+            StreamReader sr = new StreamReader("problems/day2.txt");
+            while ((report = sr.ReadLine()) != null)
+            {
+                bool dampened = false;
+                bool isSafe = true;
+                string isIncreasing = "";
+                string[] levelString = report.Split(" ");
+                int[] levelToList = Array.ConvertAll(levelString, a => int.Parse(a));
+                List<int> levelInt = new List<int>(levelToList);
+                for (int x = 0; x < levelInt.Count() - 1; x++)
+                {
+                    if (Math.Abs(levelInt[x] - levelInt[x + 1]) > 3)
+                    {
+                        isSafe = false;
+                    }
+                    if (isIncreasing == "")
+                    {
+                        if (levelInt[x] < levelInt[x + 1])
+                        {
+                            isIncreasing = "increasing";
+
+                        }
+                        else if (levelInt[x] > levelInt[x + 1])
+                        {
+                            isIncreasing = "decreasing";
+
+                        }
+                        else
+                        {
+                            isSafe = false;
+                        }
+
+                    }
+                    if (levelInt[x] < levelInt[x + 1] && isIncreasing == "increasing")
+                    {
+
+                    }
+                    else if (levelInt[x] > levelInt[x + 1] && isIncreasing == "decreasing")
+                    {
+
+                    }
+                    else
+                    {
+                        isSafe = false;
+                    }
+                    if (!isSafe && !dampened)
+                    {
+                        levelInt.RemoveAt(x);
+                        isSafe = true;
+                        dampened = true;
+                        if (x == 1)
+                        {
+                            isIncreasing = "";
+                        }
+                        if (x >= levelInt.Count())
+                        {
+                            break;
+                        }
+                        x--;
+                    }
+                    if (!isSafe)
+                    {
+                        break;
                     }
                 }
                 if (isSafe)
