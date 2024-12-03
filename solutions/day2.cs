f264e6d48f6970627983f611da5d50f6fa794911
bryan-barreto <bryan94barreto@gmail.com>
2024-12-02 20:47:40 -0500

    day 2 part 2 complete

--- solutions/day2.cs
+++ solutions/day2.cs
@@ -1,17 +1,49 @@
-using System.Diagnostics;
+using System;
 
 namespace Solutions
 {
     public static class Day2
     {
+        private static bool isSafe(int[] levelInt)
+        {
+            string isIncreasing = "";
+            for (int x = 0; x < levelInt.Count() - 1; x++)
+            {
+                if (Math.Abs(levelInt[x] - levelInt[x + 1]) > 3) { return false; }
 
-        // private class NewReport
-        // {
+                if (isIncreasing == "")
+                {
+                    if (levelInt[x] < levelInt[x + 1])
+                    {
+                        isIncreasing = "increasing";
+                    }
+                    else if (levelInt[x] > levelInt[x + 1])
+                    {
+                        isIncreasing = "decreasing";
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    continue;
+                }
 
-        //     string isSafe;
-        //     string isIncreasing;
+                if (levelInt[x] < levelInt[x + 1] && isIncreasing == "increasing")
+                {
+                    continue;
+                }
+                else if (levelInt[x] > levelInt[x + 1] && isIncreasing == "decreasing")
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
-        // }
         public static void part1()
         {
             int total = 0;
@@ -20,55 +52,14 @@ namespace Solutions
             StreamReader sr = new StreamReader("problems/day2.txt");
             while ((report = sr.ReadLine()) != null)
             {
-                bool isSafe = true;
-                string isIncreasing = "";
                 string[] levelString = report.Split(" ");
                 int[] levelInt = Array.ConvertAll(levelString, a => int.Parse(a));
-                for (int x = 0; x < levelInt.Count() - 1; x++)
-                {
-                    if (!isSafe)
-                    {
-                        continue;
-                    }
-                    if (Math.Abs(levelInt[x] - levelInt[x + 1]) > 3)
-                    {
-                        isSafe = false;
-                        continue;
-                    }
-                    if (isIncreasing == "")
-                    {
-                        if (levelInt[x] < levelInt[x + 1])
-                        {
-                            isIncreasing = "increasing";
-                        }
-                        else if (levelInt[x] > levelInt[x + 1])
-                        {
-                            isIncreasing = "decreasing";
-                        }
-                        else
-                        {
-                            isSafe = false;
-                        }
-                        continue;
-                    }
-                    if (levelInt[x] < levelInt[x + 1] && isIncreasing == "increasing")
-                    {
-                        continue;
-                    }
-                    else if (levelInt[x] > levelInt[x + 1] && isIncreasing == "decreasing")
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        isSafe = false;
-                    }
-                }
-                if (isSafe)
+                if (isSafe(levelInt))
                 {
                     total++;
                 }
             }
+
             Console.WriteLine(total);
         }
 
@@ -80,71 +71,27 @@ namespace Solutions
             StreamReader sr = new StreamReader("problems/day2.txt");
             while ((report = sr.ReadLine()) != null)
             {
-                bool dampened = false;
-                bool isSafe = true;
-                string isIncreasing = "";
                 string[] levelString = report.Split(" ");
-                int[] levelToList = Array.ConvertAll(levelString, a => int.Parse(a));
-                List<int> levelInt = new List<int>(levelToList);
-                for (int x = 0; x < levelInt.Count() - 1; x++)
-                {
-                    if (Math.Abs(levelInt[x] - levelInt[x + 1]) > 3)
-                    {
-                        isSafe = false;
-                    }
-                    if (isIncreasing == "")
-                    {
-                        if (levelInt[x] < levelInt[x + 1])
-                        {
-                            isIncreasing = "increasing";
-
-                        }
-                        else if (levelInt[x] > levelInt[x + 1])
-                        {
-                            isIncreasing = "decreasing";
-
-                        }
-                        else
-                        {
-                            isSafe = false;
-                        }
-
-                    }
-                    if (levelInt[x] < levelInt[x + 1] && isIncreasing == "increasing")
-                    {
-
-                    }
-                    else if (levelInt[x] > levelInt[x + 1] && isIncreasing == "decreasing")
-                    {
-
-                    }
-                    else
-                    {
-                        isSafe = false;
-                    }
-                    if (!isSafe && !dampened)
-                    {
-                        levelInt.RemoveAt(x);
-                        isSafe = true;
-                        dampened = true;
-                        if (x == 1)
-                        {
-                            isIncreasing = "";
-                        }
-                        if (x >= levelInt.Count())
-                        {
-                            break;
-                        }
-                        x--;
-                    }
-                    if (!isSafe)
-                    {
-                        break;
-                    }
-                }
-                if (isSafe)
+                int[] levelInt = Array.ConvertAll(levelString, a => int.Parse(a));
+                // List<int> levelInt = new List<int>(levelToList);
+                int removeAt = 0;
+                if (isSafe(levelInt))
                 {
                     total++;
+                }
+                else
+                {
+                    while (removeAt < levelInt.Count())
+                    {
+                        List<int> levelList = new List<int>(levelInt);
+                        levelList.RemoveAt(removeAt);
+                        if (isSafe(levelList.ToArray()))
+                        {
+                            removeAt = levelInt.Count();
+                            total++;
+                        }
+                        else{removeAt++;}
+                    }
                 }
             }
             Console.WriteLine(total);
